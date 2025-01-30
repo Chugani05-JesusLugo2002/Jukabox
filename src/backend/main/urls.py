9c272488f8a531871ff607dfad9f3afdfb97c355
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-01-30 11:46:49 +0000

    Add list-type urls on models

--- src/backend/main/urls.py
+++ src/backend/main/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/songs', include('songs.urls'))
+    path('api/v1/songs/', include('songs.urls')),
+    path('api/v1/artists/', include('artists.urls')),
+    path('api/v1/albums/', include('albums.urls')),
+    path('api/v1/playlists/', include('playlists.urls'))
 ]
