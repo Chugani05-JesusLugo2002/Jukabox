e3630f752e67aef9a83cf35b0494d5cac02b15b9
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-02-01 22:30:04 +0000

    Add related_name to users likes

--- src/backend/users/models.py
+++ src/backend/users/models.py
@@ -4,10 +4,10 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', default='avatars/default.jpg')
-    liked_songs = models.ManyToManyField('songs.Song', blank=True)
-    liked_albums = models.ManyToManyField('albums.Album', blank=True)
-    liked_playlist = models.ManyToManyField('playlists.Playlist', blank=True)
-    liked_artists = models.ManyToManyField('artists.Artist', blank=True)
+    liked_songs = models.ManyToManyField('songs.Song', blank=True, related_name='likes')
+    liked_albums = models.ManyToManyField('albums.Album', blank=True, related_name='likes')
+    liked_playlist = models.ManyToManyField('playlists.Playlist', blank=True, related_name='likes')
+    liked_artists = models.ManyToManyField('artists.Artist', blank=True, related_name='likes')
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
