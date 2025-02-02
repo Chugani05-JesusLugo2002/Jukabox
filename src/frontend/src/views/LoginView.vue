6bd26e24c03f56da704d6091a9aa8f680ad6037e
chugani05 <chugani205@gmail.com>
2025-02-02 21:25:24 +0000

    update on LoginView 'n' SignupView

--- src/frontend/src/views/LoginView.vue
+++ src/frontend/src/views/LoginView.vue
@@ -3,6 +3,7 @@ import type { User } from '@/components/classes/Authentication'
 import { useAPI } from '@/composables/useAPI'
 import { useAuthStore } from '@/stores/useAuth'
 import { ref } from 'vue'
+import ViewHeader from '@/components/ViewHeader.vue'
 
 const api = useAPI()
 const authStore = useAuthStore()
@@ -23,15 +24,33 @@ async function login() {
 </script>
 
 <template>
-  <form @submit.prevent="login">
-    <input type="text" name="username" id="username" v-model="username" placeholder="Username" />
-    <input
-      type="password"
-      name="password"
-      id="password"
-      v-model="password"
-      placeholder="Password"
-    />
-    <input type="submit" value="Login" />
+  <ViewHeader>Login</ViewHeader>
+  <form @submit.prevent="login" class="container mt-5 p-4 border rounded bg-light" style="max-width: 400px;">
+    <div class="mb-3 h3">
+      <label for="username" class="form-label">Username</label>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        v-model="username"
+        class="form-control"
+        placeholder="Enter your username"
+        required
+      />
+    </div>
+    <div class="mb-3 h3">
+      <label for="password" class="form-label">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        v-model="password"
+        class="form-control"
+        placeholder="Enter your password"
+        required
+      />
+    </div>
+    <button type="submit" class="btn btn-primary w-100">Login</button>
   </form>
 </template>
+
--- src/frontend/src/views/SignupView.vue
+++ src/frontend/src/views/SignupView.vue
@@ -3,6 +3,7 @@ import { useAPI } from '@/composables/useAPI'
 import { useAuthStore } from '@/stores/useAuth'
 import { ref } from 'vue'
 import type { User } from '@/components/classes/Authentication'
+import ViewHeader from '@/components/ViewHeader.vue'
 
 const api = useAPI()
 const authStore = useAuthStore()
@@ -12,6 +13,7 @@ const first_name = ref('')
 const last_name = ref('')
 const email = ref('')
 const password = ref('')
+const repeat_password = ref('')
 
 async function signup() {
   const signupData = {
@@ -20,6 +22,7 @@ async function signup() {
     last_name: last_name.value,
     email: email.value,
     password: password.value,
+    repeat_password: repeat_password.value,
   }
   const user: User | null = await api.signup(signupData)
   if (user) {
@@ -29,30 +32,87 @@ async function signup() {
 </script>
 
 <template>
-  <form @submit.prevent="signup">
-    <input type="text" name="username" id="username" v-model="username" placeholder="Username" />
-    <input
-      type="text"
-      name="first_name"
-      id="first_name"
-      v-model="first_name"
-      placeholder="first_name"
-    />
-    <input
-      type="text"
-      name="last_name"
-      id="last_name"
-      v-model="last_name"
-      placeholder="last_name"
-    />
-    <input type="email" name="email" id="email" v-model="email" placeholder="email" />
-    <input
-      type="password"
-      name="password"
-      id="password"
-      v-model="password"
-      placeholder="Password"
-    />
-    <input type="submit" value="Signup" />
+  <ViewHeader>Signup</ViewHeader>
+  <form @submit.prevent="signup" class="container mt-5 p-4 border rounded bg-light" style="max-width: 800px;">
+    <div class="row mb-3 h3">
+      <div class="col-6">
+        <label for="username" class="form-label">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          v-model="username"
+          class="form-control"
+          placeholder="Enter your username"
+          required
+        />
+      </div>
+      <div class="col-6">
+        <label for="first_name" class="form-label">First Name</label>
+        <input
+          type="text"
+          name="first_name"
+          id="first_name"
+          v-model="first_name"
+          class="form-control"
+          placeholder="Enter your first name"
+          required
+        />
+      </div>
+    </div>
+    <div class="row mb-3 h3">
+      <div class="col-6">
+        <label for="last_name" class="form-label">Last Name</label>
+        <input
+          type="text"
+          name="last_name"
+          id="last_name"
+          v-model="last_name"
+          class="form-control"
+          placeholder="Enter your last name"
+          required
+        />
+      </div>
+      <div class="col-6">
+        <label for="email" class="form-label">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          v-model="email"
+          class="form-control"
+          placeholder="Enter your email"
+          required
+        />
+      </div>
+    </div>
+    <div class="row mb-3 h3">
+      <div class="col-6">
+        <label for="password" class="form-label">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          v-model="password"
+          class="form-control"
+          placeholder="Enter your password"
+          required
+        />
+      </div>
+      <div class="col-6">
+        <label for="repeat_password" class="form-label">Repeat Password</label>
+        <input
+          type="password"
+          name="repeat_password"
+          id="repeat_password"
+          v-model="repeat_password"
+          class="form-control"
+          placeholder="Repeat your password"
+          required
+        />
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary w-100">Signup</button>
   </form>
 </template>
+
