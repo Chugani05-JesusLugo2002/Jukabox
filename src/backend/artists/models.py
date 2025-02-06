0de99336321036545530abbde4b9c8d8aa438f3a
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-02-06 15:41:19 +0000

    Profile field from artist removed

--- src/backend/artists/models.py
+++ src/backend/artists/models.py
@@ -5,7 +5,6 @@ class Artist(models.Model):
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', default='avatars/default.jpg')
     added_at = models.DateField(auto_now_add=True)
-    profile = models.OneToOneField('users.Profile', on_delete=models.SET_NULL, null=True, blank=True, related_name='artist_profile')
 
     class Meta:
         ordering = ('name',)
