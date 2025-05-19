58d4535fd8b4f9129c49fc0b0d2266d3a4125a5e
chugani05 <chugani205@gmail.com>
2025-05-19 18:45:19 +0100

    update on profileview and locales

--- src/frontend/src/views/ProfileView.vue
+++ src/frontend/src/views/ProfileView.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/useAuth'
+import { useAPI } from '@/composables/useAPI'
 import ViewHeader from '@/components/ViewHeader.vue'
+import { onMounted } from 'vue'
 
 const { user } = useAuthStore()
+const { getData } = useAPI()
+
+const likedSongs = await getData(`users/${user?.id}/liked_songs/`)
+
+
 </script>
 
 <template>
@@ -16,16 +23,18 @@ const { user } = useAuthStore()
           style="width: 250px; height: 250px; object-fit: cover;"
         />
         <div>
-          <p class="text-dark">@{{ user.username }}</p>
+          <div>
+            <p class="text-dark">@{{ user.username }}</p>
+          </div>
 
           <div class="mt-4 row text-center">
             <div class="col">
               <!-- <div class="fw-bold fs-4">{{ user.followers.length }}</div> -->
-              <small class="text-secondary">Followers:</small>
+              <small class="text-secondary">{{ $t('profile-page.followers') }}:</small>
             </div>
             <div class="col">
               <!-- <div class="fw-bold fs-4">{{ user.following.length }}</div> -->
-              <small class="text-secondary">Following:</small>
+              <small class="text-secondary">{{ $t('profile-page.following') }}:</small>
             </div>
           </div>
           
@@ -33,19 +42,42 @@ const { user } = useAuthStore()
       </div>
     </ViewHeader>
 
-    <!-- Biografía -->
     <div class="mt-4">
-      <h2 class="h5">Biography</h2>
-      <p class="text-secondary">
-        <!-- {{ user.bio || "Este usuario aún no ha escrito una biografía." }} -->
+      <p v-if="user.bio" class="text-secondary">
+        {{ user.bio }}
+      </p>
+      <p v-else>
+        {{ $t('profile-page.nobiomessage') }}
       </p>
     </div>
 
-    <!-- Estadísticas -->
     <div class="mt-4 row text-center">
       <div class="col">
         <!-- <div class="fw-bold fs-4">{{ user.songs.length }}</div> -->
-        <small class="text-secondary">Featured Songs</small>
+        <small class="text-secondary">{{ $t('profile-page.featuredsongs') }}</small>
+      </div>
+      <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div v-for="song in likedSongs" class="carousel-item">
+            <img :src="song.cover" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>{{ song.title }}</h5>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   </div>
